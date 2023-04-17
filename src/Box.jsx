492544f46dec164745b560c6c21414a6e3641cc2
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2023-04-17 09:30:46 -0300

    Events

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import { useRef } from 'react'
 
 import { useFrame } from '@react-three/fiber'
@@ -6,17 +5,19 @@ import { useFrame } from '@react-three/fiber'
 export default function Box(props) {
   const ref = useRef()
 
-  useEffect(() => {
-    console.log(ref)
-  })
-
   useFrame((_, delta) => {
     ref.current.rotation.x += 1 * delta
     ref.current.rotation.y += 0.5 * delta
   })
 
   return (
-    <mesh {...props} ref={ref}>
+    <mesh {...props} ref={ref}
+      onPointerDown={(e) => {console.log('pointer down' + e.object.name)}}
+      onPointerUp={(e) => {console.log('pointer up' + e.object.name)}}
+      onPointerOver={(e) => {console.log('pointer over' + e.object.name)}}
+      onPointerOut={(e) => {console.log('pointer out' + e.object.name)}}
+      onUpdate={(self) => {console.log(self)}}
+    >
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
